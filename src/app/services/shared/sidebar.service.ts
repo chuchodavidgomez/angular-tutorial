6770ec9f8c6be941a23db539428b7aebd7b2d72a
Jesus David Gomez Osorno <jesus.gomez@ceiba.com.co>
2020-04-09 04:18:15 -0500

    Fin de la sesion 8

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -11,7 +11,9 @@ export class SidebarService {
     submenu: [
       {titulo: 'Dashboard', url: '/dashboard'},
       {titulo: 'ProgressBar', url: '/progress'},
-      {titulo: 'graficas1', url: '/graficas1'}
+      {titulo: 'Graficas', url: '/graficas1'},
+      {titulo: 'Promesas', url: '/promesas'},
+      {titulo: 'Rxjs', url: '/rxjs'}
     ]
   }];
   constructor() { }
